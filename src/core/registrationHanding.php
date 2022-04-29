74e4a18f9452d853300b64da392f7bd1042e8e77
nima <nbayat@github.com>
2022-04-29 16:00:51 +0200

    session handling added

--- src/core/registrationHanding.php
+++ src/core/registrationHanding.php
@@ -11,6 +11,9 @@ $conn = getConnection();
 $conn->select_db('leProjet');
 $quary = "INSERT INTO users (identifiant, mail, isAdmin, password) value ('$name', '$email', 0, '$password')";
 mysqli_query($conn, $quary);
+session_start();
+$_SESSION['user'] = $name;
+header("Location: ../views/landingPage.php");
 
 function clear_input($data) {
     global $conn;
