08c0032578d924cccff423e83bcf2eab2f4efda2
nima <nbayat@github.com>
2022-05-02 12:23:56 +0200

    -

--- src/views/restaurantPage.php
+++ src/views/restaurantPage.php
@@ -27,6 +27,7 @@ function echoStar($nbStar){
 
 <html lang="fr">
 <link href="https://fonts.googleapis.com/css2?family=Material+Icons+Round" rel="stylesheet">
+<link href="css/restaurantPage.css" rel="stylesheet">
 <body class="body">
 <div class="mainContainer">
     <div class="mainContainerLeftDiv">
@@ -55,89 +56,3 @@ function echoStar($nbStar){
 </body>
 </html>
 
-<style>
-
-    .body{
-        width: 100%;
-        height: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
-
-    .mainContainer{
-        width: 95%;
-        height: 90%;
-        border-radius: 20px;
-        display: flex;
-        flex-direction: row;
-    }
-
-    .mainContainerLeftDiv {
-        flex: 3;
-    }
-
-    .mainContainerRightDiv {
-        flex: 2;
-        background-color: black;
-        padding: 20px;
-        color: white;
-        border-bottom-right-radius: 20px;
-        border-top-right-radius: 20px;
-    }
-
-    .inputBoxDiv{
-
-     }
-
-    .texteInputRow{
-        display: flex;
-        flex-direction: row;
-    }
-    
-    .inputSection{
-        flex: 4;
-        width: 100%;
-        height: 40px;
-    }
-
-    .inputNum {
-        width: 60px;
-    }
-    
-    .publierButton{
-        flex: 1;
-        background-color: orange;
-        border: black;
-    }
-
-    .avisDiv{
-        height: 500px;
-        background-color: #04AA6D;
-    }
-
-
-    .mainContainerLeftDiv img {
-        width: 100%;
-        height: 100%;
-        border-bottom-left-radius: 20px;
-        border-top-left-radius: 20px;
-    }
-
-    .mainContainerRightDiv h1{
-        text-decoration: underline;
-
-    }
-
-    .NomNoteRow {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-    }
-
-    .noteIcon{
-
-    }
-
-
-</style>
