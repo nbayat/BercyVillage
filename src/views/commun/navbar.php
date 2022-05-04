38398abb2a7c4406d0d65bdf374bd2c19b2fe87e
nima <nbayat@github.com>
2022-05-04 23:27:28 +0200

    navbar search updated

--- src/views/commun/navbar.php
+++ src/views/commun/navbar.php
@@ -5,7 +5,7 @@
 <nav class="navbar">
     <a href="/src/views/landingPage.php"><span class="material-icons-round">home</span></a>
     <form class="searchSection" method="get" action="/src/views/search.php">
-        <input type="text" placeholder="Cherchez un restaurant,..." name="searchKey" required>
+        <input type="text" placeholder="Cherchez un restaurant,une adresse...[ex: Paris note>3]" name="searchKey" required>
         <input type="submit" value="Aller" name="search">
     </form>
     <a href="/src/views/profile.php"><span class="material-icons-round">account_circle</span></a>
