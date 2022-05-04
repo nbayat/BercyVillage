0975a4dfb9872d24856698490fef185d759bc581
nima <nbayat@github.com>
2022-05-04 18:21:47 +0200

    empty espace bug removed from search function

--- src/core/searchResaultBuilder.php
+++ src/core/searchResaultBuilder.php
@@ -6,15 +6,55 @@ if (isset($_GET['searchKey'])){
     require_once 'itemBuilder.php';
     $conn = getConnection();
     $conn->select_db('leProjet');
-    $searchInput = clear_input($_GET['searchKey']);
-    $result = mysqli_query($conn, "SELECT * FROM restaurants where nom LIKE '%$searchInput%' OR adress LIKE '%$searchInput%' OR note LIKE '%$searchInput%'");
-    // $result = mysqli_query($conn, "SELECT * FROM restaurants where nom = '$searchInput'");
-    while ($row = mysqli_fetch_array($result)) {
-        $searchResaultHtml .= returnItem($row['img__Path'], intval($row['note']), $row['nom'], $row['adress']);
-    }
-    if ($result->num_rows > 0){
-        for ($i = 0; $i < 3 - $result->num_rows % 3; $i++){
-            $searchResaultHtml .= "<div class='cardItemEmpty'></div>";
+    // !!!!!!!!!!!
+    // !!!!!!!!!!!
+    // !!!!!!!!!!!
+    // Attention, nous avons besoin les chars > < = donc on peut pas sanitizer le input par ma methode
+    // clear_input definit dans src/core
+    // donc on peut avoir injection html ou xml mais on vas verifier pour sql injection
+    // en realité le projet sera plus complexe et il faut sanitizer le input contre les scripts
+    // par exemple un XSS <script>alert(1)</script> mais cette partie n'est pas à php
+    // il existe deja plusieurs methodes JS pour cela qui est pas le but du cours
+    // donc on peut ignorer injection de JS ou HTML parceque on l'utilise uniquement
+    // pour cette query SQL
+    // !!!!!!!!!!!
+    // !!!!!!!!!!!
+    // !!!!!!!!!!!
+    $searchInput = $conn->real_escape_string( $_GET['searchKey']); // Dangereux !!!!
+    // !!!!!!!!!!!
+    // !!!!!!!!!!!
+    // !!!!!!!!!!!
+    $words = preg_replace('/[0-9]+/', '', $searchInput);
+    $words = trim($words);
+    foreach (explode(' ',$words) as $word){
+        if (!str_contains($word, 'note')){
+            $query = "SELECT * FROM restaurants where nom LIKE '%{$word}%'";
+             //$query = "SELECT * FROM restaurants where nom LIKE '%{$word}%' OR adress LIKE '%$word%'";
+            // voir si existe un num
+            $noteInput = (int) filter_var($searchInput, FILTER_SANITIZE_NUMBER_INT);
+            // voir si c'est un note et identifier la condition
+            if ($noteInput != 0 && str_contains($searchInput, 'note')){
+
+                if (str_contains($searchInput, '>')) {
+                    $query .= "AND note >= $noteInput OR adress LIKE '%$word%' AND note >= $noteInput";
+                }
+                else if (str_contains($searchInput, '<')) {
+                    $query .= "AND note <= $noteInput OR adress LIKE '%$word%' AND note <= $noteInput";
+                }
+                else if (str_contains($searchInput, '=')) {
+                    echo "ok";
+                    $query .= "AND note = $noteInput OR adress LIKE '%$word%' AND note = $noteInput";
+                }
+            }
+            $result = mysqli_query($conn, $query);
+            while ($row = mysqli_fetch_array($result)) {
+                $searchResaultHtml .= returnItem($row['img__Path'], intval($row['note']), $row['nom'], $row['adress']);
+            }
+            if ($result->num_rows > 0){
+                for ($i = 0; $i < 3 - $result->num_rows % 3; $i++){
+                    $searchResaultHtml .= "<div class='cardItemEmpty'></div>";
+                }
+            }
         }
     }
 }
